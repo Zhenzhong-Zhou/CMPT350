8fad15a85313e651af0305ebf046e5997acfc4f7
Zhenzhong Zhou <bob.zhou@usask.ca>
2020-01-30 11:05:39 -0600

    Finished Page Part

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,11 +1,26 @@
 const express = require("express");
 const router = express.Router();
+const Page = require("../../models/admin/page");
 
 /*
  * GET admin index route
  */
-router.get("/", (req, res) => {
-    res.render("admin/index");
+router.get("/", async (req, res) => {
+    // let pages;
+    // try {
+    //     pages = Page.find().sort({sorting: "-1"}).limit(5).exec()
+    // }catch (e) {
+    //     pages = []
+    // }
+    // res.render("admin/index", {pages: pages});
+    try {
+        const pages = await Page.find({}).sort({sorting: 1}).exec();
+        res.render("admin/index", {
+            pages: pages
+        });
+    }catch (e) {
+        res.render("admin");
+    }
 });
 
 module.exports = router;
